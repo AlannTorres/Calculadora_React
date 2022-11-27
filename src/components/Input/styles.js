67a55fa6dc9de2a68046e5a202bc1fa198b6ea41
AlannTorres <alanvictor.torres@gmail.com>
2022-11-27 17:35:11 -0300

    First Commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input {
+        padding: 0 10px;
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        borde: 0;
+        
+        display: flex;
+        felx-direction: column;
+        align: flex-end;
+        color: #FFFFFF;
+    }
+`
